460edc32bd7840ecb814fb121f1e08cfa20162cf
brusnitsyn <brusnitsyn@github.com>
2025-02-04 01:14:38 +0900

    Элемент рабочей области

---
+++ resources/js/Components/Workspace/WorkspaceItem.vue
@@ -0,0 +1,87 @@
+<script setup>
+import {IconLock} from "@tabler/icons-vue"
+import {Link} from '@inertiajs/vue3'
+const props = defineProps({
+    href: {
+        type: String,
+    },
+    header: {
+        type: String
+    },
+    icon: {
+
+    },
+    type: {
+        type: String,
+        default: 'default'
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    disabledReason: {
+        type: String
+    }
+})
+
+const headerClass = computed(() => {
+    const classes = []
+    switch (props.type) {
+        case 'default':
+            classes.push('text-green-600')
+        break
+        case 'error':
+            classes.push('text-red-600')
+        break
+    }
+
+    return classes
+})
+
+const iconClass = computed(() => {
+    const classes = []
+    switch (props.type) {
+        case 'default':
+            classes.push('text-green-600')
+            break
+        case 'error':
+            classes.push('text-red-600')
+            break
+    }
+
+    return classes
+})
+
+
+</script>
+
+<template>
+    <div class="relative overflow-clip">
+        <Link :href="href">
+            <NCard hoverable class="cursor-pointer">
+                <template #header>
+                    <div v-bind:class="headerClass">
+                        {{ header }}
+                    </div>
+                </template>
+                <template #header-extra>
+                    <NIcon :component="icon" size="28" :class="iconClass" />
+                </template>
+            </NCard>
+        </Link>
+        <div v-if="disabled" class="absolute inset-0 bg-black bg-opacity-10 backdrop-blur-[4px] rounded-[3px]">
+            <div class="flex items-center justify-center h-full">
+                <NSpace vertical :size="0" item-class="flex" align="center" inline justify="center">
+                    <NIcon :component="IconLock" size="28" />
+                    <NText strong>
+                      {{ disabledReason }}
+                    </NText>
+                </NSpace>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Workspace.vue
+++ resources/js/Pages/Workspace.vue
@@ -1,44 +1,30 @@
 <script setup>
-import {Link} from '@inertiajs/vue3'
 import AppLayout from '@/Layouts/AppLayout.vue';
 import { IconCirclePlus, IconTableDashed, IconDoorExit } from '@tabler/icons-vue'
+import WorkspaceItem from "@/Components/Workspace/WorkspaceItem.vue";
+import {usePage} from "@inertiajs/vue3";
+const page = usePage()
+const hasSelectDepartment = computed(() => page.props.activeDepartment)
 </script>
 
 <template>
     <AppLayout title="Dashboard">
         <NFlex vertical :size="20" class="max-w-xl mx-auto h-full" justify="space-between">
-            <Link :href="route('request.create')">
-                <NCard hoverable class="cursor-pointer">
-                    <template  #header>
-                        <div class="text-[#18A058]">
-                            Создать запрос на транспортировку
-                        </div>
-                    </template>
-                    <template #header-extra>
-                        <NIcon :component="IconCirclePlus" size="28" class="text-[#18A058]" />
-                    </template>
-                </NCard>
-            </Link>
-            <NCard hoverable class="cursor-pointer">
-                <template  #header>
-                    <div class="text-[#18A058]">
-                        Мои запросы
-                    </div>
-                </template>
-                <template #header-extra>
-                    <NIcon :component="IconTableDashed" size="28" class="text-[#18A058]" />
-                </template>
-            </NCard>
-            <NCard hoverable class="cursor-pointer">
-                <template  #header>
-                    <div class="text-[#d03050]">
-                        Выход
-                    </div>
-                </template>
-                <template #header-extra>
-                    <NIcon :component="IconDoorExit" size="28" class="text-[#d03050]" />
-                </template>
-            </NCard>
+            <WorkspaceItem header="Создать запрос на транспортировку"
+                           :href="route('request.create')"
+                           :icon="IconCirclePlus"
+                           :disabled="hasSelectDepartment === null"
+                           disabled-reason="Выберите МО"
+            />
+            <WorkspaceItem header="Мои запросы"
+                           :href="route('request.create')"
+                           :icon="IconTableDashed"
+            />
+            <WorkspaceItem header="Выход"
+                           :href="route('request.create')"
+                           :icon="IconDoorExit"
+                           type="error"
+            />
         </NFlex>
     </AppLayout>
 </template>
