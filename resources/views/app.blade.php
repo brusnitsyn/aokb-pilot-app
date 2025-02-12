063e00eed1883132cb5c0b9e2bd02a116db58ff3
brusnitsyn <brusnitsyn@github.com>
2025-02-12 12:37:16 +0900

    PWA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
@@ -10,6 +10,7 @@
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
+        <link rel="manifest" href="/manifest.webmanifest" />
     </head>
     <body class="font-sans antialiased">
         @inertia
