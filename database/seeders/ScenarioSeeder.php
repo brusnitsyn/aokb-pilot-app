ca822fb395ae81c345454fc0963f071aaef0c2e2
brusnitsyn <andrew.brusnitsyn@gmail.com>
2025-02-26 11:46:43 +0900

    Добавлены сценарии для ОКС

--- database/seeders/ScenarioSeeder.php
+++ database/seeders/ScenarioSeeder.php
@@ -18,6 +18,10 @@ class ScenarioSeeder extends Seeder
         $scenarios = [
             [ 'name' => 'А. Прогрессирующая стенокардия', 'score' => 0 ],
             [ 'name' => 'Б. ИМ без подъема с ST', 'score' => 2 ],
+            [ 'name' => 'В. ИМ с подъемом ST давностью >48', 'score' => 2 ],
+            [ 'name' => 'Г. ИМ с подъемом ST давностью >12<48 ч.', 'score' => 2 ],
+            [ 'name' => 'Д. ИМ с подъемом ST после ТЛТ', 'score' => 2 ],
+            [ 'name' => 'Е. ИМ с подъемом ST давностью до 12 ч. без ТЛТ', 'score' => 2 ],
         ];
 
         Scenario::insert($scenarios);
