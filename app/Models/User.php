bbc65e1bb02cfb33c5faa55b395cfe57881d5f3e
brusnitsyn <andrew.brusnitsyn@gmail.com>
2025-02-27 14:48:23 +0900

    Исправление пропажи активного МО на главном экране

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Cookie;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
@@ -74,7 +75,9 @@ class User extends Authenticatable
 
     public function myDepartment()
     {
-        $myDepartmentId = json_decode(\request()->cookie('myDepartment'));
+        $myDepartmentId = Cookie::get('myDepartment')
+            ? Cookie::get('myDepartment')
+            : Cookie::get('senderDepartment');
 
         if ($myDepartmentId === null && $this->role->slug === 'ROLE_ADMIN')
             return null;
