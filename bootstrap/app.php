df937f38daa5c0c9801912f5735d5b4f58b65832
brusnitsyn <brusnitsyn@gmail.com>
2025-08-22 12:30:36 +0900

    Добавлена интеграция с Sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,5 +25,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        \Sentry\Laravel\Integration::handles($exceptions);
     })->create();
